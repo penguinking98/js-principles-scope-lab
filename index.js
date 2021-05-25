2114cfa7c5a7a824dc27ccf78514d6deefd02473
Ben Kramer <benkramer614@gmail.com>
2021-05-25 11:20:30 -0400

    commit tree 443ca9025583185cbb98b92201de9f8cedf9d1aa

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your solution in this file!
+var customerName = 'bob';
+var bestCustomer = 'not bob';
+function upperCaseCustomerName(){
+}
+function setBestCustomer(){
+}
+function overwriteBestCustomer(){
+}
+function changeLeastFavoriteCustomer(){
+
+}
